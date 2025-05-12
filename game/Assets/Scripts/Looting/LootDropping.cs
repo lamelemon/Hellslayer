330a0c83894c937430ce992460dd2b5e34ad97a9
Eetu Juhani Laaksolinna <s4laee03@students.osao.fi>
2025-05-12 11:32:02 +0300

    ei valmis vielä mut teen kotona

--- game/Assets/Scripts/Looting/LootDropping.cs
+++ game/Assets/Scripts/Looting/LootDropping.cs
@@ -15,29 +15,34 @@ public class LootDropping : MonoBehaviour
         // Check if lootItems is not null and has items
         if (lootItems.Length > 0)
         {
-            // Randomly select a loot item from the array
-            GameObject lootItem = LootItem.InstantiateLootItem(LootItem.FindItemByRandomWeight(lootItems), transform.position);
-            Collider[] colliders = lootItem.GetComponents<Collider>();
-            Rigidbody rb = lootItem.GetComponent<Rigidbody>();
-            rb.isKinematic = true;
+            GameObject[] droppedItems = LootItem.InstantiateLootItems(LootItem.FindItemByRandomWeight(lootItems), transform.position);
+            Collider[][] colliders = LootItem.GetColliders(droppedItems);
+            Rigidbody[] rb = LootItem.GetRigidbodies(droppedItems);
+            foreach (Rigidbody rigidbody in rb)
+            {
+                rigidbody.isKinematic = true; // Set all rigidbodies to kinematic
+            }
         
-            StartCoroutine(DelayedStuff(lootItem, rb, colliders));
+            //StartCoroutine(DelayedStuff(lootItem, rb, colliders));
         }
     }
 
-    private float GetHeight(Collider[] colliders)
+    private float GetHeight(Collider[][] colliders)
     {
-        foreach (Collider collider in colliders)
+        foreach (Collider[] colliderArray in colliders)
         {
-            if (collider.bounds.size.y > 0)
+            foreach (Collider collider in colliderArray)
             {
-                return collider.bounds.size.y;
+                if (collider.bounds.size.y > 0)
+                {
+                    return collider.bounds.size.y;
+                }
             }
         }
 
         return 0f; // Return zero if no collider found
     }
-    IEnumerator DelayedStuff(GameObject obj, Rigidbody rb, Collider[] colliders)
+/*    IEnumerator DelayedStuff(GameObject[] obj, Rigidbody[] rb, Collider[][] colliders)
     {
         yield return new WaitForFixedUpdate();
         obj.transform.position = transform.position + GetHeight(colliders) / 2 * Vector3.up; // Adjust the position to be above the enemy
@@ -45,7 +50,7 @@ public class LootDropping : MonoBehaviour
         rb.AddForce(new(UnityEngine.Random.Range(2f, 5f), 5f, UnityEngine.Random.Range(2f, 5f)), ForceMode.Impulse); // Add upward force to the loot item
         lootItems = null;
         transform.gameObject.SetActive(false); // Deactivate the enemy object
-    }
+    }*/
 }
 
 [Serializable]
@@ -54,8 +59,11 @@ public class LootItem
     [Tooltip("The prefab of the loot item. This should be a prefab that can be instantiated in the game world.")]
     public GameObject itemPrefab;
     [Tooltip("The weight of the item. Higher weight means more likely to drop.")]
+    [Range(0, 100000)]
     public int weight;
-    [Tooltip("The range of the amount of items that can drop (min inclusive, max inclusive).")]
+    
+    [Tooltip("The range of the amount of items that can drop (min inclusive, max inclusive). PLEASE DON'T SET TO AN INSANELY HIGH NUMBER.")]
+    [Range(1, 100)]
     public Vector2Int dropAmountRange;
 
     public int GetRandomDropAmount()
@@ -106,5 +114,35 @@ public class LootItem
     public static GameObject InstantiateLootItem(LootItem lootItem, Vector3 position)
     {
         return GameObject.Instantiate(lootItem.itemPrefab, position, Quaternion.identity);
+    }
+
+    public static GameObject[] InstantiateLootItems(LootItem lootItem, Vector3 position)
+    {
+        GameObject[] lootItems = new GameObject[lootItem.GetRandomDropAmount()];
+        for (int i = 0; i < lootItems.Length; i++)
+        {
+            lootItems[i] = InstantiateLootItem(lootItem, position);
+        }
+        return lootItems;
+    }
+
+    public static Collider[][] GetColliders(GameObject[] lootItems)
+    {
+        Collider[][] colliders = new Collider[lootItems.Length][];
+        for (int i = 0; i < lootItems.Length; i++)
+        {
+            colliders[i] = lootItems[i].GetComponents<Collider>();
+        }
+        return colliders;
+    }
+
+    public static Rigidbody[] GetRigidbodies(GameObject[] lootItems)
+    {
+        Rigidbody[] rigidbodies = new Rigidbody[lootItems.Length];
+        for (int i = 0; i < lootItems.Length; i++)
+        {
+            rigidbodies[i] = lootItems[i].GetComponent<Rigidbody>();
+        }
+        return rigidbodies;
     }
 }
