f5f5b0f416810b745908c85900eda782be096564
Poppi75 <kananenjohannes@gmail.com>
2025-05-12 08:52:41 +0300

    en muista

--- game/Assets/Scripts/Player/PlayerItemInteraction.cs
+++ game/Assets/Scripts/Player/PlayerItemInteraction.cs
@@ -11,8 +11,12 @@ public class PlayerInteraction : MonoBehaviour
 
     [SerializeField] private PlayerInputManager getInput;
     [SerializeField] private Transform handPosition; // Reference to the player's hand position
+    [SerializeField] private string crimson_sword = "crimson_sword"; // Reference to a tag
+    [SerializeField] private string katana = "katana"; // Reference to a tag
 
-    private TestItem currentlyHeldItem; // Reference to the currently held item
+    public TestItem currentlyHeldItem; // Reference to the currently held item
+
+    // public CrimsonSword currentlyHeldItem2; // Reference to the currently held item
 
     private void Start()
     {
@@ -125,3 +129,4 @@ public class PlayerInteraction : MonoBehaviour
         currentlyHeldItem = null; // Clear the reference to the held item
     }
 }
+
