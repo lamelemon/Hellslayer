37fce6ea39d8d79d35af26e630045a9c5afc2c34
bazzdragon <ossi.heikkinen5000@gmail.com>
2025-05-14 11:43:33 +0300

    main menu ja options alotusksia

--- game/Assets/Scripts/Main Menu/TextHoverEffect.cs
+++ game/Assets/Scripts/Main Menu/TextHoverEffect.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
 
-public class TextHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class TextHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public TextMeshProUGUI text;
+
     public Color normalColor = Color.white;
-    public Color hoverColor = Color.yellow;
+    public Color hoverColor = new Color(5f, 5f, 0f, 1f); // Bright HDR yellow
+    public Color clickColor = new Color(0f, 5f, 5f, 1f); // Bright HDR cyan
 
-    void Start() => text.color = normalColor;
+    private bool isHovered = false;
 
-    public void OnPointerEnter(PointerEventData eventData) => text.color = hoverColor;
+    void Start()
+    {
+        text.color = normalColor;
+    }
 
-    public void OnPointerExit(PointerEventData eventData) => text.color = normalColor;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovered = true;
+        text.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovered = false;
+        text.color = normalColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        text.color = clickColor;
+    }
 }
